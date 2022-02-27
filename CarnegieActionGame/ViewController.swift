c96fe8a4075b94571ea29f947c4628e40e209c49
seungwon-yoo <qwer15417@gmail.com>
2022-02-27 21:28:13 +0900

    20220227

--- CarnegieActionGame/ViewController.swift
+++ CarnegieActionGame/ViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
